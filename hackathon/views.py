692ae8df8369df236ecebed4009065fea4498ea5
César <cesarlpb@gmail.com>
2024-06-11 17:19:32 +0200

    Ejemplo de context

--- hackathon/views.py
+++ hackathon/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def inicio(request):
-  print("request:", request)
-  print("Has llamado a inicio")
-  return render(request, "inicio.html")
+  context = {
+    "nombre": "pepe",
+    "apellido": "la rana"
+  }
+  # conectar a db
+  # lógica
+  # formatear
+  return render(request, "inicio.html", context=context)
